ddd03d7dd5a32bd3f08314058736f73dbf1261aa
Jefferson Carvalho Santana <jsantana@MACCIT016778.local>
2020-01-15 08:59:40 -0300

    Initial commit

--- func.js
+++ func.js
@@ -1,23 +1,77 @@
-module.exports = {
-  setDirectory: (fs, path) => {
-    let dirApproved = '../img_approved';
-    let dirCurrent = '../img_current';
-    let dirReproved = '../img_reproved';
+const fs = require('fs');
+const path = require('path');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 
-    if (!fs.existsSync(dirApproved)) fs.mkdirSync(dirApproved);
-    if (!fs.existsSync(dirCurrent)) fs.mkdirSync(dirCurrent);
-    if (!fs.existsSync(dirReproved)) fs.mkdirSync(dirReproved);
+const dirApproved = '../img_approved';
+const dirCurrent = '../img_current';
+const dirReproved = '../img_reproved';
 
-    return {
-      directoryPathApproved: path.join(__dirname, dirApproved),
-      directoryPathCurrent: path.join(__dirname, dirCurrent),
-      directoryPathReproved: path.join(__dirname, dirReproved),
-    }
-  },
-  
-  getDirectories: (fs, path) => {
-    return fs.readdirSync(path).filter(function (file) {
-      return fs.statSync(path + '/' + file).isDirectory();
-    });
-  }
+const configDirectories = () => {
+  if (!fs.existsSync(dirApproved)) fs.mkdirSync(dirApproved);
+  if (!fs.existsSync(dirCurrent)) fs.mkdirSync(dirCurrent);
+  if (!fs.existsSync(dirReproved)) fs.mkdirSync(dirReproved);
 }
+
+const getDirectory = {
+  directoryPathApproved: path.join(__dirname, dirApproved),
+  directoryPathCurrent: path.join(__dirname, dirCurrent),
+  directoryPathReproved: path.join(__dirname, dirReproved),
+}
+
+const getSubDirectories = (path) => {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
+}
+
+const verifyDiff = (pngFiles, dir) => {
+  pngFiles.forEach(function (file) {
+    const imgCurrent = PNG.sync.read(
+      fs.readFileSync(path.join(dir, file)));
+    const imgApproved = PNG.sync.read(
+      fs.readFileSync(path.join(dir, file)));
+    const {width, height} = imgCurrent;
+    const diff = new PNG({width, height});
+
+    let qtdDiff = pixelmatch(imgCurrent.data, imgApproved.data, diff.data, width, height, {threshold:0});
+    if(qtdDiff > 0) {
+      console.log('NOK'.red, ' - '.red, file.red);
+      fs.writeFileSync(path.join(getDirectory.directoryPathApproved, 'diff-' + file), PNG.sync.write(diff));
+    } else {
+      console.log(' OK'.blue, ' - ', file);
+    }
+  });
+}
+
+const eachDirectory = (pathForEach) => {
+  getSubDirectories(pathForEach).forEach(function(dir) {
+    fs.readdir(path.join(pathForEach, dir), function (err, files) {
+      let pngFiles = files.filter(el => /\.png$/.test(el));
+      let directories = files.filter(el => /^[^.]+$/.test(el));
+
+      if(pngFiles.length > 0) {
+        let splitCaminho = path.join(pathForEach, dir).split('/');
+        console.log(
+          splitCaminho[splitCaminho.length-2] + '/' +
+          splitCaminho[splitCaminho.length-1] + ':'
+        );
+
+        verifyDiff(pngFiles, path.join(pathForEach, dir));
+
+        console.log('\n\n');
+      }
+
+      for (var prop in directories) {
+        let kk = path.join(pathForEach, dir, directories[prop]);
+        eachDirectory(kk);
+      }
+    });
+  });
+}
+
+exports.configDirectories = configDirectories;
+exports.getDirectory = getDirectory;
+exports.getSubDirectories = getSubDirectories;
+exports.verifyDiff = verifyDiff;
+exports.eachDirectory = eachDirectory;
--- index.js
+++ index.js
@@ -1,49 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const PNG = require('pngjs').PNG;
-const pixelmatch = require('pixelmatch');
 const colors = require('colors');
-const FUNC = require('./func.js');
+const {
+  configDirectories,
+  getDirectory,
+  getSubDirectories,
+  verifyDiff,
+  eachDirectory
+} = require('./func.js');
 
-let pngFiles;
+configDirectories();
 
-const setDirectory = FUNC.setDirectory(fs, path);
-
-const verifyDiff = (pngFiles, dir) => {
-  pngFiles.forEach(function (file) {
-    const imgCurrent = PNG.sync.read(
-      fs.readFileSync(path.join(path.join(setDirectory.directoryPathCurrent, dir), file)));
-    const imgApproved = PNG.sync.read(
-      fs.readFileSync(path.join(path.join(setDirectory.directoryPathApproved, dir), file)));
-    const {width, height} = imgCurrent;
-    const diff = new PNG({width, height});
-
-    let qtdDiff = pixelmatch(imgCurrent.data, imgApproved.data, diff.data, width, height, {threshold:0});
-    if(qtdDiff > 0) {
-      console.log('NOK'.red, ' - '.red, file.red);
-      fs.writeFileSync(path.join(setDirectory.directoryPathReproved, 'diff-' + file), PNG.sync.write(diff));
-    } else {
-      console.log(' OK'.blue, ' - ', file);
-    }
-  });
-}
-
-const directory = FUNC.getDirectories(fs, setDirectory.directoryPathCurrent);
-
-directory.forEach(function(dir) {
-  fs.readdir(path.join(setDirectory.directoryPathCurrent, dir), function (err, files) {
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-
-    pngFiles = files.filter(el => /\.png$/.test(el));
-
-    if (pngFiles.length === 0) return;
-
-    console.log(dir, ' - Relatórios de teste:');
-
-    verifyDiff(pngFiles, dir);
-
-    console.log('--- FIM ---');
-  });
-});
+console.log('Relatório de teste:\n');
+eachDirectory(getDirectory.directoryPathCurrent);
